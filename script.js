1d1412ea9be0b87dc41b7d8807c21edcc341d9d9
Nardin Lachowski <nardin@live.com>
2020-12-06 14:16:27 -0500

    added date functionality and output

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=$
 let queryURLuv = `http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=${ApiKey}`;
 let queryURLforecast = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt={5}&appid=${ApiKey}`;
 
+// Today's Date JS function
+var d = new Date();
+var day = d.getDate();
+var month = d.getMonth() + 1;
+var year = d.getFullYear();
+document.write(month + "/" + day + "/" + year);
+
+// GET functionality weather in selected city + HTML output
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -12,7 +20,7 @@ $.ajax({
     console.log(response)
     const tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-    $(".city-selected").html(`<h1>${response.name} (${response.weather[0].description})</h1>`);
+    $(".city-selected").html(`<h1>${response.name} (${month}/${day}/${year}) (${response.weather[0].description})</h1>`);
     $(".wind").text(`Wind Speed: ${response.wind.speed}`);
     $(".humidity").text(`Humidity: ${response.main.humidity}`);
     $(".temp").append(`Temperature (F): ${tempF.toFixed(2)}`);
@@ -20,18 +28,10 @@ $.ajax({
 
 });
 
+// GET functionality for weather forecast of selected city + HTML output
 $.ajax ({
     url: queryURLforecast,
     method: "GET"
 }).then(function(response){
     console.log("test 123");
 });
-
-
-
-
-
-
-
-
-// Click Listener 
